ed60a2ae5caa80329a2fcd531902fbeefe69aa4e
Sarathi <Sarathik86@gmail.com>
2023-03-25 00:13:22 +0530

    Rest Assured Projects

---
+++ RestAssuredProject/src/test/java/org/testing/testcases/TestCasesSample.java
@@ -0,0 +1,23 @@
+package org.testing.testcases;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class TestCasesSample {
+
+	public static void main(String[] args) {
+			//Test Case Sample to get the records using BDD Framework
+		Response res = 
+		given()
+		.contentType(ContentType.JSON)
+		.when()
+		.get("http://localhost:3000/APIStudentList");
+		
+		System.out.println("The Status code: "+res.statusCode());
+		System.out.println("The response data is below: ");
+		System.out.println(res.asString());
+	}
+
+}
