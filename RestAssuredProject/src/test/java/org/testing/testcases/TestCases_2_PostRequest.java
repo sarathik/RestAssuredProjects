25f4bb64a335a0b50361e524ce7eb0c619f81e49
Sarathi <Sarathik86@gmail.com>
2023-04-01 20:02:27 +0530

    New code for the Post method

---
+++ RestAssuredProject/src/test/java/org/testing/testcases/TestCases_2_PostRequest.java
@@ -0,0 +1,18 @@
+package org.testing.testcases;
+
+public class TestCases_2_PostRequest {
+
+	public static void main(String[] args) {
+		// This Code is for the Post Request
+		
+		// There are 3 ways
+		//1. org.json library
+		//2. external json file
+		//3.usomg pojo classes
+		
+		
+	
+
+	}
+
+}
