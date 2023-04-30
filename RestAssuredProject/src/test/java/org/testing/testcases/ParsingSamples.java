20bcf22e8009856c91e3e195a2661afbd713867e
Sarathi <Sarathik86@gmail.com>
2023-04-30 21:23:00 +0530

    Parsing the simple value from the Response

---
+++ RestAssuredProject/src/test/java/org/testing/testcases/ParsingSamples.java
@@ -0,0 +1,30 @@
+package org.testing.testcases;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class ParsingSamples {
+
+	public static void main(String[] args) {
+		// Get particular object value from the JSON Response
+		
+		Response res =
+				given()
+				.contentType(ContentType.JSON)
+				.when()
+				.get("http://localhost:3000/APIStudentList");
+		
+		//Syntax : objectName.searchKeyName
+		//array : arrayName[indexnumer].objectName.searchKey
+		
+		//Parsing from the simple JSON
+		String idName = res.jsonPath().get("[1].name");
+		System.out.println(idName);
+		
+		
+
+	}
+
+}
